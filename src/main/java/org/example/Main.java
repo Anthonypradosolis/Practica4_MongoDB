acd1a2062515c1e0e973e24cbb7dd3042ebd7c91
Anthonypradosolis <apradosolis@danielcastelao.org>
2025-02-27 14:06:15 +0100

    subimos todo para realizarlo en casa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,24 @@
 package org.example;
 
+import com.mongodb.client.MongoCollection;
+import org.example.MongoDB.BaseDatos.MongoDBConnection;
+import org.example.MongoDB.Metodos.MetodosMongo;
+
 public class Main {
     public static void main(String[] args) {
 
+        MongoDBConnection mongoDBConnection = new MongoDBConnection();
 
+        mongoDBConnection.connect();
+
+        MongoCollection collection = mongoDBConnection.getCollection("Habitats");
+
+        mongoDBConnection.getCollection("Habitats");
+        mongoDBConnection.getCollection("Pinguinos");
+
+        MetodosMongo metodosMongo = new MetodosMongo();
+
+        metodosMongo.importarXML("habitats.xml",collection);
 
 
 
--- src/main/java/org/example/MongoDB/Metodos/MetodosMongo.java
+++ src/main/java/org/example/MongoDB/Metodos/MetodosMongo.java
@@ -1,29 +1,28 @@
 package org.example.MongoDB.Metodos;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import org.bson.Document;
-
+import com.fasterxml.jackson.dataformat.xml.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 public class MetodosMongo {
 
-    public void importarJSON(String filePath, MongoCollection<Document> collection){
-        ObjectMapper mapper = new ObjectMapper();
-        try{
-            Map<String, Object>[] jsonObject = mapper.readValue(new File(filePath), Map[].class);
+    public void importarXML(String filePath, MongoCollection<Document> collection) {
+        XmlMapper xmlMapper = new XmlMapper();
+        try {
+            List<Map<String, Object>> xmlData = xmlMapper.readValue(new File(filePath), List.class);
 
-            for(Map<String, Object> doc : jsonObject){
+            for (Map<String, Object> doc : xmlData) {
                 collection.insertOne(new Document(doc));
             }
-            System.out.println("Datos importados correctamente");
-        } catch (IOException e){
+            System.out.println("Datos importados correctamente desde XML");
+        } catch (IOException e) {
             System.out.println("Error al importar los documentos: " + e.getMessage());
         }
-
     }
 
 
@@ -37,6 +36,15 @@ public class MetodosMongo {
             }
         }catch (Exception e){
             System.err.println("Error al leer los documentos: " + e.getMessage());
+        }
+    }
+
+    public void actualizarXML(MongoCollection<Document> collection, Document filtro, Document nuevosDatos) {
+        try {
+            collection.updateOne(filtro, new Document("$set", nuevosDatos));
+            System.out.println("Documento actualizado correctamente");
+        } catch (Exception e) {
+            System.err.println("Error al actualizar el documento: " + e.getMessage());
         }
     }
 }
